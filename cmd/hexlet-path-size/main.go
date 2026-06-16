ba7286e2f1f11820ce0e9937c2ce945f830c58cb
Konstantin Korovin <cosmo.stan@yandex.com>
2026-06-16 16:48:44 +0300

    feat: implement help output for cli

--- cmd/hexlet-path-size/main.go
+++ cmd/hexlet-path-size/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v3"
+)
 
 func main() {
-	fmt.Println("Hello from Hexlet!")
+	cmd := &cli.Command{
+		Name:  "hexlet-path-size",
+		Usage: "print size of a file or directory",
+		Action: func(ctx context.Context, c *cli.Command) error {
+			fmt.Println("Hello from Hexlet!")
+			return nil
+		},
+	}
+
+	err := cmd.Run(context.Background(), os.Args)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
