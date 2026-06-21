a586013dcdcff1994d4141c9608d4aa7adb3ed97
Konstantin Korovin <cosmo.stan@yandex.com>
2026-06-21 21:40:56 +0300

    test: add test for recursive calculating

--- path_size_test.go
+++ path_size_test.go
@@ -9,34 +9,34 @@ import (
 
 func TestGetPathSize(t *testing.T) {
 	t.Run("file", func(t *testing.T) {
-		result, err := GetPathSize("testdata/test.txt", false, false)
+		result, err := GetPathSize("testdata/test.txt", false, false, false)
 		require.NoError(t, err)
 		require.Equal(t, "6B", result)
 	})
 
 	t.Run("file with human-readable size", func(t *testing.T) {
-		result, err := GetPathSize("testdata/test.txt", true, false)
+		result, err := GetPathSize("testdata/test.txt", false, true, false)
 		require.NoError(t, err)
 		require.Equal(t, "6B", result)
-		result, err = GetPathSize("testdata/0_YISbBYJg5hkJGcQd.png", true, false)
+		result, err = GetPathSize("testdata/0_YISbBYJg5hkJGcQd.png", false, true, false)
 		require.NoError(t, err)
 		require.Equal(t, "22.5KB", result)
 	})
 
 	t.Run("directory", func(t *testing.T) {
-		result, err := GetPathSize("testdata", false, false)
+		result, err := GetPathSize("testdata", false, false, false)
 		require.NoError(t, err)
 		require.Equal(t, "293830B", result)
 	})
 
 	t.Run("directory with human-readable size", func(t *testing.T) {
-		result, err := GetPathSize("testdata", true, false)
+		result, err := GetPathSize("testdata", false, true, false)
 		require.NoError(t, err)
 		require.Equal(t, "293.8KB", result)
 	})
 
 	t.Run("nonexistent path", func(t *testing.T) {
-		result, err := GetPathSize("unknown", false, false)
+		result, err := GetPathSize("unknown", false, false, false)
 		require.ErrorIs(t, err, os.ErrNotExist)
 		require.Empty(t, result)
 	})
@@ -45,18 +45,29 @@ func TestGetPathSize(t *testing.T) {
 		dir := t.TempDir()
 		require.NoError(t, os.Chmod(dir, 0o000))
 
-		result, err := GetPathSize(dir, false, false)
+		result, err := GetPathSize(dir, false, false, false)
 		require.Error(t, err)
 		require.Empty(t, result)
 	})
 
 	t.Run("directory with hidden files", func(t *testing.T) {
-		result, err := GetPathSize("testdata/hidden", false, false)
+		result, err := GetPathSize("testdata/hidden", false, false, false)
 		require.NoError(t, err)
 		require.Equal(t, "0B", result)
 
-		result, err = GetPathSize("testdata/hidden", false, true)
+		result, err = GetPathSize("testdata/hidden", false, false, true)
 		require.NoError(t, err)
 		require.Equal(t, "11B", result)
+	})
+
+	t.Run("recursive directory size", func(t *testing.T) {
+		result, err := GetPathSize("testdata/recursive", true, false, false)
+		require.NoError(t, err)
+		require.Equal(t, "55B", result)
+
+		// including hidden files and directories
+		result, err = GetPathSize("testdata/recursive", true, false, true)
+		require.NoError(t, err)
+		require.Equal(t, "89B", result)
 	})
 }
